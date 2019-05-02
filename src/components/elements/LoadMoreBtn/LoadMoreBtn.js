031bd1ac3ded326f595ca9cf7bc1e3af3d489246
Hari Basnet <harikrishna.basnet@yahoo.com>
2019-05-02 18:31:44 +0300

    more changes

---
+++ src/components/elements/LoadMoreBtn/LoadMoreBtn.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './LoadMoreBtn.css';
+
+const LoadMoreBtn = (props) => {
+    return (
+        <div>
+            loadmorebtbb is also awesome 
+        </div>
+    )
+}
+
+export default LoadMoreBtn;
---
+++ src/components/elements/MovieThumb/MovieThumb.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MovieThumb.css';
+
+const MovieThumb = (props) =>{
+    return(
+        <div>
+            moviethumb 
+        </div>
+    )
+}
+
+export default MovieThumb;
